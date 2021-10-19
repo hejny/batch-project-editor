7d3fa99617e61f0d1545fa6f65bab13462faf9e3
Pavol Hejny <me@pavolhejny.com>
2021-10-19 13:05:09 +0200

    Iterate through whole dir

--- change-commits.sh
+++ change-commits.sh
@@ -1,18 +1,26 @@
-cd ../../townsgame/Towns2;
+cd ../../townsgame/
 
-git filter-branch --env-filter '
-    WRONG_EMAIL="ph@towns.cz"
-    NEW_NAME="Pavol Hejný"
-    NEW_EMAIL="me@pavolhejny.com"
+for d in */ ; do
+    echo "$d";
+    cd $d;
 
-    if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
-    then
-        export GIT_COMMITTER_NAME="$NEW_NAME"
-        export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
-    fi
-    if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
-    then
-        export GIT_AUTHOR_NAME="$NEW_NAME"
-        export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
-    fi
-' --tag-name-filter cat -- --branches --tags;
+    git filter-branch --force --env-filter '
+        WRONG_EMAIL="ph@towns.cz"
+        NEW_NAME="Pavol Hejný"
+        NEW_EMAIL="me@pavolhejny.com"
+
+        if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+        then
+            export GIT_COMMITTER_NAME="$NEW_NAME"
+            export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+        fi
+        if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+        then
+            export GIT_AUTHOR_NAME="$NEW_NAME"
+            export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+        fi
+    ' --tag-name-filter cat -- --branches --tags;
+
+    git push --force;
+    cd ..;
+done
