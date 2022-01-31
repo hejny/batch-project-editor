c40100d19e63081b49605660a575657334ef7a45
Pavol Hejny <me@pavolhejny.com>
2022-01-31 22:48:55 +0100

    Working 🔺 Audit dependencies

--- src/workflows/auditDependencies/auditDependencies.ts
+++ src/workflows/auditDependencies/auditDependencies.ts
@@ -1,26 +1,48 @@
+import spaceTrim from 'spacetrim';
+import { execCommand } from '../../utils/execCommand/execCommand';
 import { IWorkflowOptions } from '../IWorkflow';
 
-export async function badges({
+export async function auditDependencies({
+    projectPath,
     modifyPackage,
     runCommand,
     commit,
 }: IWorkflowOptions): Promise<void> {
-    await runCommand('npm install');
-    await runCommand('npm audit fix');
+    await execCommand({ cwd: projectPath, crashOnError: false, command: 'npm install' });
+    await execCommand({ cwd: projectPath, crashOnError: false, command: 'npm audit fix' });
 
-    // Removing prefixes before versioning - we just want "10.4.0" not "^10.4.0" @see https://youtu.be/ctkGh7RpgQ8
+    await commit(
+        spaceTrim(`
+          🔺 Audit dependencies
+
+          Executed command \`npm audit fix\`
+      `),
+    );
+
+    // Note: Removing prefixes before versioning - we just want "10.4.0" not "^10.4.0" @see https://youtu.be/ctkGh7RpgQ8
     await modifyPackage((packageContent) => {
         for (const type of ['devDependencies', 'dependencies']) {
             for (const dependency in packageContent[type]) {
-                const match =
-                    /([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/.exec(
-                        packageContent[type][dependency],
-                    );
-                packageContent[type][dependency] = match[0];
+                const match = packageContent[type][dependency].match(
+                    /([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/,
+                );
+
+                if (!match) {
+                    continue;
+                }
+
+                packageContent[type][dependency] = match![0];
             }
         }
         return packageContent;
     });
 
-    await commit('🔺 Audit dependencies');
+    await commit(
+        spaceTrim(`
+          🔺 Removing dependencies prefixes
+
+          Removing prefixes before versioning - we just want "10.4.0" not "^10.4.0"
+          For more reasoning watch https://youtu.be/ctkGh7RpgQ8
+    `),
+    );
 }
