b77ee940efe675849ad75c2ac313785cb85a3168
Pavol Hejny <me@pavolhejny.com>
2022-02-01 20:51:00 +0100

    CLonning the repositories via js

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,20 @@
+import { ConfigChecker } from 'configchecker';
+import dotenv from 'dotenv';
+import path from 'path';
 import { badges } from './workflows/badges/badges';
 import { IWorkflow } from './workflows/IWorkflow';
 
-export const BASE_PATH = 'C:/Users/me/autowork';
-//export const BASE_PATH = 'G:/Backups/github';
 export const WORKFLOWS: IWorkflow[] = [
     //auditDependencies,
     badges,
     //lines /* !!! List all and allow to pick via CLI flags */,
 ];
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+const config = ConfigChecker.from(process.env);
+
+export const BASE_PATH = config.get('BASE_PATH').required().value;
+
+export const GITHUB_USERNAME = config.get('GITHUB_USERNAME').required().value;
+export const GITHUB_ORGANIZATIONS = config.get('GITHUB_ORGANIZATIONS').list().required().value;
+export const GITHUB_TOKEN = config.get('GITHUB_TOKEN').required().value;
---
+++ src/utils/findAllProjectsRemote.ts
@@ -0,0 +1,30 @@
+import { Octokit } from '@octokit/rest';
+import { GITHUB_ORGANIZATIONS, GITHUB_TOKEN, GITHUB_USERNAME } from '../config';
+
+export async function findAllProjectsRemote(): Promise<Record<string, string[]>> {
+    const octokit = new Octokit({ key: GITHUB_TOKEN });
+
+    const aggregatedData: Record<string, string[]> = {};
+
+    {
+        const { data } = await octokit.rest.repos.listForUser({
+            username: GITHUB_USERNAME,
+            per_page: 100,
+        });
+        aggregatedData[GITHUB_USERNAME] = data.filter(({ archived }) => !archived).map(({ html_url }) => html_url);
+    }
+
+    for (const org of GITHUB_ORGANIZATIONS) {
+        const { data } = await octokit.rest.repos.listForOrg({
+            org,
+            per_page: 100,
+        });
+        aggregatedData[org] = data.filter(({ archived }) => !archived).map(({ html_url }) => html_url);
+    }
+
+    return aggregatedData;
+}
+
+/**
+ * TODO: Iterate thorugh pages
+ */
