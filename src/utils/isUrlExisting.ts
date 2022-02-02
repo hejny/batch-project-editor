5516705702124d27be3f1fdace2e229e0e650347
Pavol Hejny <me@pavolhejny.com>
2022-02-02 21:52:09 +0100

    Note: When packagequality.com looks at the package for the first time, it returns a 504 status code

--- src/utils/isUrlExisting.ts
+++ src/utils/isUrlExisting.ts
@@ -4,6 +4,12 @@ export async function isUrlExisting(url: string): Promise<boolean> {
     try {
         const response = await fetch(url);
         console.info('isUrlExisting', url, response.status);
+
+        if (response.status >= 500) {
+            // Note: When packagequality.com looks at the package for the first time, it returns a 504 status code
+            return isUrlExisting(url);
+        }
+
         return response.status < 400;
     } catch (error) {
         return false;
