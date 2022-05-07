03d8e7a9f9ff9299bca9289fee826e4a5c3b63a6
Pavol Hejny <me@pavolhejny.com>
2022-05-07 10:01:53 +0200

    Draft of terminalsVersion

---
+++ src/workflows/terminals-version/terminalsVersion.ts
@@ -0,0 +1,30 @@
+import { IWorkflowOptions } from '../IWorkflow';
+
+export async function terminalsVersion({ commit }: IWorkflowOptions): Promise<void> {
+    // TODO: Implement
+    //await commit('🤸⛹️ Authors in package.json');
+    // TODO: Implement
+    /*
+    {
+      "name": "🔼👑 Release major version",
+      "command": "npm version major",
+      "onlySingle": true,
+      "focus": true
+  },
+  {
+      "name": "🔼🚀 Release minor version",
+      "command": "npm version minor",
+      "onlySingle": true,
+      "focus": true
+  },
+  {
+      "name": "🔼🩹 Release patch version",
+      "command": "npm version patch",
+      "onlySingle": true,
+      "focus": true
+  }
+  */
+}
+
+// !!! Auto detect all commands
+// !!! Auto clone repos before run
