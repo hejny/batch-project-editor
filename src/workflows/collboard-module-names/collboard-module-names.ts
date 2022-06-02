036622b751edad1bad6b80330702edb004ef4591
Pavol Hejny <me@pavolhejny.com>
2022-06-02 17:19:00 +0200

    Draft of 📦 Propper package names for Collboard

---
+++ src/workflows/collboard-module-names/collboard-module-names.ts
@@ -0,0 +1,6 @@
+import { IWorkflowOptions } from '../IWorkflow';
+
+export async function collboardModuleNames({ commit }: IWorkflowOptions): Promise<void> {
+    // TODO: Implement
+    await commit('📦 Propper package names for Collboard');
+}
