f3479872fd67594ae5f1304bd9781fa0194ac377
Pavol Hejny <me@pavolhejny.com>
2022-06-05 00:18:29 +0200

    Branches workflow

--- src/workflows/branches-remove-merged/branches-remove-merged.ts
+++ src/workflows/branches-remove-merged/branches-remove-merged.ts
@@ -1,6 +1,19 @@
 import { IWorkflowOptions } from '../IWorkflow';
 
-export async function branchesRemoveMerged({ commit }: IWorkflowOptions): Promise<void> {
-    // TODO: Implement
- 
+export async function branchesRemoveMerged({ runCommand }: IWorkflowOptions): Promise<void> {
+    /*
+    Note: This command makes no sence because the merged branchech will not be downloaded at the first place.
+    TODO: Maybe also push the deletion to the remote?
+
+
+    const branches = (await runCommand('git branch --merged')).split('\n');
+
+    for (const branch of branches) {
+        if (branch.startsWith('*')) {
+            continue;
+        }
+
+        await runCommand(`git branch -D ${branch}`);
+    }
+    */
 }
--- src/workflows/branches-update-features/branches-update-features.ts
+++ src/workflows/branches-update-features/branches-update-features.ts
@@ -1,5 +1,35 @@
 import { IWorkflowOptions } from '../IWorkflow';
 
-export async function branchesUpdateFeatures({ commit }: IWorkflowOptions): Promise<void> {
-    // TODO: Implement
+/**
+ * Go through not merged branches and update them with the latest commit the main.
+ * - It will take only recent branches = branches with commits in the last month
+ * - It will take only feature branches = branches that start with `feature/`
+ */
+export async function branchesUpdateFeatures({ commit, runCommand }: IWorkflowOptions): Promise<void> {
+  
+
+    await runCommand('git fetch origin --prune --tags --no-recurse-submodules');
+
+    const branches = (await runCommand('git branch --no-merged')).split('\n');
+    const fetureBranches = branches.filter((branch) => branch.startsWith('feature/'));
+    const recentBranches = fetureBranches.filterAsync(async (branch) => {
+        const lastCommit = (await runCommand(`git log -1 --format=%ct ${branch}`)).trim();
+        const lastCommitDate = new Date(parseInt(lastCommit, 10) * 1000);
+        const now = new Date();
+        return now.getMonth() - lastCommitDate.getMonth() <= 1;
+    });
+
+    console.log({
+        branches,
+        fetureBranches,
+        recentBranches,
+    });
+
+    /*
+    for (const branch of fetureBranches) {
+        await runCommand(`git checkout ${branch}`);
+        await runCommand('git pull');
+
+    }
+    */
 }
