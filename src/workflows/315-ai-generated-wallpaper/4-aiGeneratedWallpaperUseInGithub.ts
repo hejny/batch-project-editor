bb7519c03667d2ce91435532b919fa7000a952fb
Pavol Hejny <me@pavolhejny.com>
2022-08-21 03:17:50 +0200

    aiGeneratedWallpaperUseInGithub is working with shrinking images to fit Github size

--- src/workflows/315-ai-generated-wallpaper/4-aiGeneratedWallpaperUseInGithub.ts
+++ src/workflows/315-ai-generated-wallpaper/4-aiGeneratedWallpaperUseInGithub.ts
@@ -1,7 +1,11 @@
+import chalk from 'chalk';
+import { stat, unlink } from 'fs/promises';
 import glob from 'glob-promise';
 import { join } from 'path';
-import { IWorkflowOptions, WorkflowResult } from '../IWorkflow';
+import sharp from 'sharp';
+import { forTime } from 'waitasecond';
 import { clickOnText } from '../../utils/clickOnText';
+import { IWorkflowOptions, WorkflowResult } from '../IWorkflow';
 import { getGithubPage, prepareGithubPage } from './utils/githubPage';
 
 export async function aiGeneratedWallpaperUseInGithub({
@@ -16,6 +20,8 @@ export async function aiGeneratedWallpaperUseInGithub({
     // TODO: !!! Choose current better than just the first
     const wallpaperCurrentPath = (await glob(join(wallpaperGalleryPath, '*.png')))[0];
 
+    // !!!!!! Take image from README
+
     if (!wallpaperCurrentPath) {
         return skippingBecauseOf(`No wallpaper yet`);
     }
@@ -25,8 +31,30 @@ export async function aiGeneratedWallpaperUseInGithub({
     await githubPage.goto(`${projectUrl}/settings`, { waitUntil: 'networkidle0' });
 
     (async () => {
-        const fileChooser = await githubPage.waitForFileChooser();
-        await fileChooser.accept([wallpaperCurrentPath]);
+        const originalPath = wallpaperCurrentPath;
+        const shrinkedPath = join(process.cwd(), '.tmp', 'shrikened.png');
+
+        try {
+            const fileChooser = await githubPage.waitForFileChooser();
+
+            for (let k = 1; true; k = k * 0.9) {
+                const width = Math.ceil(1280 * k);
+                const height = Math.ceil(640 * k);
+
+                console.info(chalk.gray(`Shrinking image to ${width}x${height}`));
+
+                await sharp(originalPath).resize({ width, height }).png({ compressionLevel: 9 }).toFile(shrinkedPath);
+
+                if ((await stat(shrinkedPath)).size < 1000000) {
+                    break;
+                }
+            }
+
+            await fileChooser.accept([shrinkedPath]);
+            await forTime(1000 * 15);
+        } finally {
+            await unlink(shrinkedPath);
+        }
     })();
 
     await clickOnText(githubPage, 'Edit');
