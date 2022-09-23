110a51adaaef42e914d1bc0f6b450f86ebcb79cd
Pavol Hejny <me@pavolhejny.com>
2022-09-23 22:29:59 +0200

    isUrlExisting recursion protection and waiting

--- src/utils/isUrlExisting.ts
+++ src/utils/isUrlExisting.ts
@@ -1,13 +1,24 @@
 import fetch from 'node-fetch';
+import { forTime } from 'waitasecond';
 
-export async function isUrlExisting(url: string): Promise<boolean> {
+/**
+ * Check if the given URL is a valid and contains some content
+ *
+ * @param url {string}
+ * @returns {boolean}
+ */
+export async function isUrlExisting(url: string, _recursion = 0): Promise<boolean> {
+    if (_recursion >= 10) {
+        return false;
+    }
     try {
         const response = await fetch(url);
         console.info('isUrlExisting', url, response.status);
 
         if (response.status >= 500) {
             // Note: When packagequality.com looks at the package for the first time, it returns a 504 status code
-            return isUrlExisting(url);
+            await forTime(1000);
+            return isUrlExisting(url, _recursion + 1);
         }
 
         return response.status < 400;
