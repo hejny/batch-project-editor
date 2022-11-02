e764918171cdf572948832b7e1f7ba26c92e8f0f
Pavol Hejny <me@pavolhejny.com>
2022-11-02 20:24:24 +0100

    Making stats

--- src/aggregators/gitHours/gitHours.ts
+++ src/aggregators/gitHours/gitHours.ts
@@ -5,25 +5,20 @@ import { IAggregator, IAggregatorOptions } from '../IAggregator';
 
 interface IGitHoursResult {
     allCommitsCount: number;
-    filteredCommitsCount: number;
+    leadingCommitsCount: number;
+    commitsCount: number;
+    time: moment.Duration;
 }
 
 export class gitHours implements IAggregator<IGitHoursResult> {
     public initial = {
         allCommitsCount: 0,
-        filteredCommitsCount: 0,
+        leadingCommitsCount: 0,
+        commitsCount: 0,
+        time: moment.duration({ minutes: 0 }),
     };
 
     public async run({ projectPath }: IAggregatorOptions) {
-        /*
-        !!! rEMOVE
-        const commitsString = await execCommandOnProject(
-            `git log --pretty=format:'{%n  "commit": "%H",%n  "abbreviated_commit": "%h",%n  "tree": "%T",%n  "abbreviated_tree": "%t",%n  "parent": "%P",%n  "abbreviated_parent": "%p",%n  "refs": "%D",%n  "encoding": "%e",%n  "subject": "%s",%n  "sanitized_subject_line": "%f",%n  "body": "%b",%n  "commit_notes": "%N",%n  "verification_flag": "%G?",%n  "signer": "%GS",%n  "signer_key": "%GK",%n  "author": {%n    "name": "%aN",%n    "email": "%aE",%n    "date": "%aD"%n  },%n  "commiter": {%n    "name": "%cN",%n    "email": "%cE",%n    "date": "%cD"%n  }%n},'`,
-        );
-        const commits = JSON.parse(commitsString);
-
-        */
-
         const allCommits = await gitlog({
             repo: projectPath,
             number: 100000 /* <- TODO: Want Infinite */,
@@ -43,23 +38,34 @@ export class gitHours implements IAggregator<IGitHoursResult> {
 
         // !!! Exculude = skip BPE
 
-        const filteredCommits = allCommits.filter(({ body }) => !body.includes(BATCH_PROJECT_EDITOR_COMMIT_SIGNATURE));
+        const commits = allCommits.filter(({ body }) => !body.includes(BATCH_PROJECT_EDITOR_COMMIT_SIGNATURE));
 
+        let leadingCommitsCount = 0;
+        let time = moment.duration({ minutes: 0 });
         let lastDate: null | Date = null;
-        for (const commit of filteredCommits) {
+        for (const commit of commits) {
             const { subject, authorDate, body } = commit; /* <- TODO: Destruct in for loop */
             const currentDate = new Date(authorDate);
 
             if (lastDate) {
-                const duration = moment.duration({ milliseconds: lastDate.getTime() - currentDate.getTime() });
+                let commitTime = moment.duration({ milliseconds: lastDate.getTime() - currentDate.getTime() });
 
-                if (duration.asHours() > 2 /* <- TODO: Configurable */) {
+                if (
+                    commitTime.asHours() > 2
+                    /* <- TODO: Configurable */
+                    /* <- TODO: Add different limit through different daytimes (4h day, 2h night) */
+                ) {
                     console.log('___');
-                    // !!! TODO: LERP average duration of commit according to additions/deletions
+                    leadingCommitsCount++;
+                    commitTime = moment.duration({
+                        minutes: 30 /* <- !!! TODO: LERP average duration of commit according to additions/deletions */,
+                    });
                 }
 
+                time.add(commitTime);
+
                 // console.log(duration.asHours());
-                console.log(subject, ' took ', duration.humanize(), ` changed ${commit.files.length} file`, body);
+                console.log(subject, ' took ', commitTime.humanize(), ` changed ${commit.files.length} file`, body);
 
                 // !!! TODO: Analyze that duration is not totally different (40%) from avarage
 
@@ -72,20 +78,29 @@ export class gitHours implements IAggregator<IGitHoursResult> {
 
         return {
             allCommitsCount: allCommits.length,
-            filteredCommitsCount: filteredCommits.length,
+            leadingCommitsCount,
+            commitsCount: commits.length,
+            time,
         };
     }
 
     public join(a: IGitHoursResult, b: IGitHoursResult) {
         return {
             allCommitsCount: a.allCommitsCount + b.allCommitsCount,
-            filteredCommitsCount: a.filteredCommitsCount + b.filteredCommitsCount,
+            leadingCommitsCount: a.leadingCommitsCount + b.leadingCommitsCount,
+            commitsCount: a.commitsCount + b.commitsCount,
+            time: a.time.add(b.time),
         };
         // TODO: return joinObjects(a,b);
     }
 
-    public print(value: IGitHoursResult) {
-        return value;
+    public print(result: IGitHoursResult) {
+        const { time } = result;
+        return {
+            ...result,
+            time: time.humanize(),
+            timeAsHours: Math.round(time.asHours() * 100) / 100,
+        };
     }
 }
 
