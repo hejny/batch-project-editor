44edc8786ac278b7d2d3b3cf94b9db6b226c065f
Pavol Hejny <me@pavolhejny.com>
2023-03-20 18:42:49 +0100

    Send a first message to BingChat

--- src/workflows/500-todos/writeAnotations.ts
+++ src/workflows/500-todos/writeAnotations.ts
@@ -1,12 +1,16 @@
 import { ElementHandle } from 'puppeteer-core';
 import spaceTrim from 'spacetrim';
-import { forEver } from 'waitasecond';
+import { forEver, forTime } from 'waitasecond';
 import { IWorkflowOptions, WorkflowResult } from '../IWorkflow';
 import { getChatBingPage, prepareChatBingPage } from './utils/chatBingPage';
 
 export async function writeAnotations({ modifyFiles, commit }: IWorkflowOptions): Promise<WorkflowResult> {
     const chatBingPage = getChatBingPage();
 
+    /**
+     * Finds a searchbox element inside multiple shadowRoot layers
+     * DRY [0]
+     */
     const searchboxElementHandle = (await chatBingPage.evaluateHandle(() => {
         function traverse(
             node: Node,
@@ -19,8 +23,7 @@ export async function writeAnotations({ modifyFiles, commit }: IWorkflowOptions)
                 return result;
             }
 
-            if (node.nodeType == 1) {
-                // (Element node)
+            if (node.nodeType == 1 /* <- Element node */) {
                 for (var i = 0; i < node.childNodes.length; i++) {
                     const result = traverse(node.childNodes[i], depth + 1, callback);
 
@@ -28,8 +31,8 @@ export async function writeAnotations({ modifyFiles, commit }: IWorkflowOptions)
                         return result;
                     }
                 }
-            } else if (node.nodeType == 3) {
-                // (Text node)
+            } else if (node.nodeType == 3 /* <- Text node */) {
+                // Note: Text node can not be traversed
             }
 
             if (node instanceof HTMLElement && node.shadowRoot) {
@@ -66,7 +69,69 @@ export async function writeAnotations({ modifyFiles, commit }: IWorkflowOptions)
         });
     })) as ElementHandle<HTMLElement>;
 
-    searchboxElementHandle.type('Hello');
+    /**
+     * Finds a searchbox element inside multiple shadowRoot layers
+     * DRY [0]
+     */
+    const submitElementHandle = (await chatBingPage.evaluateHandle(() => {
+        function traverse(
+            node: Node,
+            depth: number,
+            callback: (node: Node, depth: number) => HTMLElement | null,
+        ): HTMLElement | null {
+            const result = callback(node, depth);
+
+            if (result !== null) {
+                return result;
+            }
+
+            if (node.nodeType == 1 /* <- Element node */) {
+                for (var i = 0; i < node.childNodes.length; i++) {
+                    const result = traverse(node.childNodes[i], depth + 1, callback);
+
+                    if (result !== null) {
+                        return result;
+                    }
+                }
+            } else if (node.nodeType == 3 /* <- Text node */) {
+                // Note: Text node can not be traversed
+            }
+
+            if (node instanceof HTMLElement && node.shadowRoot) {
+                for (var i = 0; i < node.shadowRoot.childNodes.length; i++) {
+                    const result = traverse(node.shadowRoot.childNodes[i], depth + 1, callback);
+
+                    if (result !== null) {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        return traverse(window.document.body, 0, (node: Node, depth: number) => {
+            if (!(node instanceof HTMLElement)) {
+                return null;
+            }
+
+            const element = node;
+
+            if (!(element.tagName === 'BUTTON' && element.ariaLabel === 'Submit')) {
+                return null;
+            }
+
+            const submitElement = element;
+
+            return submitElement;
+        });
+    })) as ElementHandle<HTMLElement>;
+
+    await searchboxElementHandle.type('Hello');
+    await forTime(500);
+    await submitElementHandle.click();
+
+    // TODO: Scrape <cib-message><cib-shared/>
 
     await forEver();
 
