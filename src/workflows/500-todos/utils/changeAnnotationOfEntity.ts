bdbe6270c41c5c6391691e07ec70affa3f7168cf
Pavol Hejny <me@pavolhejny.com>
2023-03-24 16:38:33 +0100

    Add annotations to changeAnnotationOfEntity

--- src/workflows/500-todos/utils/changeAnnotationOfEntity.ts
+++ src/workflows/500-todos/utils/changeAnnotationOfEntity.ts
@@ -1,8 +1,29 @@
+/**
+ * The options object for the changeAnnotationOfEntity function.
+ */
 interface ChangeAnnotationOfEntityParams {
+    /**
+     * The source code to update.
+     */
     source: string;
+
+    /**
+     * The name of the entity to update the annotation for.
+     */
     entityName: string;
+
+    /**
+     * The new annotation to add to the entity.
+     */
     annotation: string;
 }
+
+/**
+ * Changes the annotation of a specified entity in the given source code.
+ * @param {ChangeAnnotationOfEntityParams} options - The options object.
+ * @returns {string} The updated source code with the new annotation added to the specified entity.
+ * @throws {Error} Throws an error if the specified entity is not found in the given source code.
+ */
 export function changeAnnotationOfEntity({ source, entityName, annotation }: ChangeAnnotationOfEntityParams): string {
     const lines = source.split('\n');
     const entityTypes = ['const', 'let', 'var', 'class', 'interface', 'type', 'function'];
