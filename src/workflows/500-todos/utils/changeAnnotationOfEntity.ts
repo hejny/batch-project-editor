a01ba1db5942961f91b26ee0d227ac4bc0adbf9d
Pavol Hejny <me@pavolhejny.com>
2023-03-24 16:12:43 +0100

    Working on changeAnnotationOfEntity
    Much better implementation

--- src/workflows/500-todos/utils/changeAnnotationOfEntity.ts
+++ src/workflows/500-todos/utils/changeAnnotationOfEntity.ts
@@ -5,45 +5,25 @@ interface ChangeAnnotationOfEntityParams {
 }
 export function changeAnnotationOfEntity({ source, entityName, annotation }: ChangeAnnotationOfEntityParams): string {
     const lines = source.split('\n');
-    const outputLines: string[] = [];
-    let foundEntity = false;
-    let inExistingAnnotation = false;
-    let existingAnnotation = '';
-    for (const line of lines) {
-        if (line.includes(`/**`) && !foundEntity) {
-            inExistingAnnotation = true;
-            continue;
+    const entityTypes = ['const', 'let', 'var', 'class', 'interface', 'type', 'function'];
+    const entityLineIndex = lines.findIndex((line) =>
+        entityTypes.some((type) => line.includes(`${type} ${entityName}`)),
+    );
+    if (entityLineIndex === -1) return source;
+
+    const annotationLines = annotation.split('\n').map((line) => ` * ${line}`);
+    annotationLines.unshift('/**');
+    annotationLines.push(' */');
+
+    let startInsertIndex = entityLineIndex;
+    if (lines[entityLineIndex - 1] === ' */') {
+        startInsertIndex -= 2;
+        while (lines[startInsertIndex] !== '/**') {
+            startInsertIndex--;
         }
-        if (inExistingAnnotation) {
-            if (line.includes(`*/`)) {
-                inExistingAnnotation = false;
-            } else {
-                existingAnnotation += line.trim().slice(2).trim();
-            }
-            continue;
-        }
-        if (
-            line.includes(`const ${entityName} =`) ||
-            line.includes(`let ${entityName} =`) ||
-            line.includes(`var ${entityName} =`) ||
-            line.includes(`function ${entityName}(`) ||
-            line.includes(`async function ${entityName}(`) ||
-            line.includes(`class ${entityName}`) ||
-            line.includes(`interface ${entityName}`) ||
-            line.includes(`type ${entityName} =`)
-        ) {
-            if (existingAnnotation !== annotation) {
-                outputLines.push(`/**`);
-                outputLines.push(` * ${annotation}`);
-                outputLines.push(` */`);
-                existingAnnotation = '';
-            }
-            foundEntity = true;
-        }
-        outputLines.push(line);
+        lines.splice(startInsertIndex, entityLineIndex - startInsertIndex);
     }
-    if (!foundEntity) {
-        throw new Error(`Could not find entity with name: ${entityName}`);
-    }
-    return outputLines.join('\n');
+
+    lines.splice(startInsertIndex, 0, ...annotationLines);
+    return lines.join('\n');
 }
