e861bf9434d7a4a97904233a7964c2a51b19090e
Pavol Hejny <me@pavolhejny.com>
2023-05-02 00:54:12 +0200

    💭 Write annotations
    @see ChatBing from Tue May 02 2023
    
    🔼 This commit was automatically generated by [Batch project editor](https://github.com/hejny/batch-project-editor) using workflow onceWriteAnnotations

--- src/workflows/315-ai-generated-wallpaper/utils/trigger/getStatusOfButton.ts
+++ src/workflows/315-ai-generated-wallpaper/utils/trigger/getStatusOfButton.ts
@@ -4,13 +4,11 @@ import { forPlay } from '../../../../utils/forPlay';
 export type ButtonStatus = 'BLANK' | 'TRIGGERED' | 'UNKNOWN';
 
 /**
- * Gets the status of a button ⁘
- *
- * @async
- * @function getStatusOfButton
- * @param {ElementHandle<HTMLButtonElement>} elementHandle - The handle to the button element.
- * @param {boolean} isLogged - Whether the user is logged in or not.
- * @returns {Promise<ButtonStatus>} The status of the button.
+ * Gets the status of a button element based on its background color ⁘
+ * 
+ * @param {ElementHandle<HTMLButtonElement>} elementHandle - The handle of the button element.
+ * @param {boolean} isLogged - A flag that indicates whether the user is logged in or not.
+ * @returns {Promise<ButtonStatus>} A promise that resolves to the status of the button element.
  */
 export async function getStatusOfButton(
     elementHandle: ElementHandle<HTMLButtonElement>,
