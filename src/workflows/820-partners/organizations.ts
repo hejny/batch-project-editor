6f062da57e3c49e7f6a5f89716dc135e4c8c88e2
Pavol Hejny <me@pavolhejny.com>
2024-04-17 17:20:31 +0200

    WebGPT partner

--- src/workflows/820-partners/organizations.ts
+++ src/workflows/820-partners/organizations.ts
@@ -39,7 +39,7 @@ const ORGANIZATIONS = {
         url: new URL(
             `https://webgpt.cz/?partner=ph&utm_medium=referral&utm_source=github-readme&utm_campaign=partner-ph`,
         ),
-        logoSrc: new URL(`https://webgpt.cz/_next/static/media/webgpt.white.7e7069eb.png`),
+        logoSrc: new URL(`https://webgpt.cz/_next/static/media/webgpt-blue.e2bf1fff.png`),
     },
 
     //----
